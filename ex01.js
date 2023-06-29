54499646c74a7f408fb29cd11ef89a43ffef6c75
Akyssa Eduarda <110582028+akyssaeduarda@users.noreply.github.com>
2023-06-29 15:23:12 -0300

    atividade fixação 1

---
+++ ex01.js
@@ -0,0 +1,20 @@
+//1°
+var numero1 = new Number(300);
+var numero2 = new Number(999);
+var numero3 = new Number(21)
+
+console.log(numero1, numero2, numero3);
+
+
+//2°
+console.log(Number.EPSILON);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+
+//3°
+var valor = new Number(150)
+console.log(valor)
+console.log(valor.toString())
+
+
